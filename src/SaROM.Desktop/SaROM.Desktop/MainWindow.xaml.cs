20851ee35dcac35a0daa8d5d5eee0c8bbc8b3812
Oliver Appel <o.appel@topmotive.eu>
2019-06-19 00:03:20 +0200

    Create operation  management view

--- src/SaROM.Desktop/SaROM.Desktop/MainWindow.xaml.cs
+++ src/SaROM.Desktop/SaROM.Desktop/MainWindow.xaml.cs
@@ -6,38 +6,51 @@ using System.Windows.Controls;
 
 namespace SaROM.Desktop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Login login = null;
+    private LoginManager loginManager = null;
+
+    public MainWindow()
     {
-        private Login login = null;
-        private LoginManager loginManager = null;
+      InitializeComponent();
+      // RegisterLoginControl();
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            RegisterLoginControl();
-        }
-
-        private void OnSuccessfullLogin(object sender, EventArgs e)
-        {
-            var overview = new Overview();
-            SetContent(overview);
-        }
-
-        private void RegisterLoginControl()
-        {
-            loginManager = new LoginManager();
-            login = new Login(loginManager);
-            login.LoginSuccessfull += OnSuccessfullLogin;
-
-            SetContent(login);
-        }
-
-        private void SetContent(Control overview)
-        {
-            cc_Main.Content = overview;
-        }
+      // Avoid login every time during development
+      var operation = new Operation();
+      SetContent(operation); 
     }
+
+    private void OnSuccessfullLogin(object sender, EventArgs e)
+    {
+      var overview = new Overview();
+      overview.OperationManagement_Click += CallOperationManagement;
+
+      SetContent(overview);
+    }
+
+    private void CallOperationManagement(object sender, EventArgs e)
+    {
+      var operation = new Operation();
+
+      SetContent(operation);
+    }
+
+    private void RegisterLoginControl()
+    {
+      loginManager = new LoginManager();
+      login = new Login(loginManager);
+      login.LoginSuccessfull += OnSuccessfullLogin;
+
+      SetContent(login);
+    }
+
+    private void SetContent(Control overview)
+    {
+      cc_Main.Content = overview;
+    }
+  }
 }
--- src/SaROM.Desktop/SaROM.Desktop/Views/Overview.xaml.cs
+++ src/SaROM.Desktop/SaROM.Desktop/Views/Overview.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace SaROM.Desktop.Views
 {
-    /// <summary>
-    /// Interaction logic for OverviewControl.xaml
-    /// </summary>
-    public partial class Overview : UserControl
+  /// <summary>
+  /// Interaction logic for OverviewControl.xaml
+  /// </summary>
+  public partial class Overview : UserControl
+  {
+    public Overview()
     {
-        public Overview()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    public event EventHandler OperationManagement_Click;
+
+    private void Btn_OperationManagement_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+      OperationManagement_Click.Invoke(this, null);
+    }
+  }
 }
